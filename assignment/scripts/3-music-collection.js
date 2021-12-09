c989e1efcd653a0dd217ecaf786c9b0d96b9404c
bryant.nupdal <nupdalbryant@icloud.com>
2021-12-09 16:18:10 -0600

    update before adding 'tracks' to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -4,7 +4,7 @@ function addToCollection (title, artist, yearPublished) {
   let albumInfo = { title: title,
                     artist: artist,
                     yearPublished: yearPublished
-     // Left of colen is the key | To the right is the value
+     // Left of colon is the key | To the right is the value
                   } //End of Object 'albumInfo'
   collection.push(albumInfo);
   return collection;
@@ -39,7 +39,26 @@ function findByArtist(input){
   }// End for loop
     return results;
 }// End findByArtist Function
-console.log(findByArtist('Luke Combs'));
-console.log(findByArtist('Two Friends'));
-console.log(findByArtist('Machine Gun Kelly'));
-console.log(findByArtist('Filmore'));
+console.log(findByArtist('Luke Combs')); //Should appear twice
+console.log(findByArtist('Two Friends')); // Should not appear because it is not in the collection
+console.log(findByArtist('Machine Gun Kelly')); //Should appear once
+console.log(findByArtist('Filmore')); // Should appear once
+
+function search( artist, year ){
+  let searchArray = [];
+  console.log('Currently searching for: ', artist, ' and ', year);
+  for (let i = 0; i < collection.length; i++){
+    const temp = collection[i];
+    if (artist === temp.artist && year === temp.yearPublished){
+      searchArray.push(temp);
+      console.log('The matching albums in the collection are: ');
+    }// End of first If
+    if (artist == null || artist == '' && year == null || year == ''){
+      console.log(temp);
+    }// End of second If
+  }//End of for loop
+  return searchArray;
+}//End of Function
+console.log(search('Ray Charles', '1957'));
+console.log(search('Filmore', '2020'));
+console.log(search());
